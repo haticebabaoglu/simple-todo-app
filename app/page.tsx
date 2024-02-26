a90dffab24aff77f1eaa708ec7bd6571e9331f4e
haticebabaoglu <haticebaba63@gmail.com>
2024-02-26 12:09:57 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import AddTask from "./components/AddTask";
 
 
 export default function Home() {
   return (
-    <main className="max-w-4xl bg-red-400" >
-      <h1> next js</h1>
+    <main className="max-w-4xl mx-auto mt-4" >
+     <div className="text-center my-5 flex flex-col gap-4 ">
+     <h1 className="text-2xl font-bold" > Todo List</h1>
+     <AddTask/>
+     </div>
     </main>
   );
 }
