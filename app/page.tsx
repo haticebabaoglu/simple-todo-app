7b4bd168bcf1591a573ce92ca50b9c047a80d945
haticebabaoglu <haticebaba63@gmail.com>
2024-02-29 22:56:15 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
 
-export default function Home() {
-  const tasks = getAllTodos()
+export default async function Home() {
+  const tasks = await getAllTodos()
   return (
     <main className="max-w-4xl mx-auto mt-4" >
      <div className="text-center my-5 flex flex-col gap-4 ">
