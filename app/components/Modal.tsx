ab10c6fffb136bd4426522744243f3af286efc35
haticebabaoglu <haticebaba63@gmail.com>
2024-03-04 22:39:43 +0300

    update

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -7,7 +7,7 @@ interface ModalProps {
   const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
     return (
       <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
-        <div className='modal-box relative'> deneme
+        <div className='modal-box relative'>
           <label
             onClick={() => setModalOpen(false)}
             className='btn btn-sm btn-circle absolute right-2 top-2'
