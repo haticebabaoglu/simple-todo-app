1ae950c839ce84e0f0d841ebc78215f145930b9e
haticebabaoglu <haticebaba63@gmail.com>
2024-03-05 01:09:11 +0300

    bug fixed

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -11,12 +11,16 @@ const AddTask = () => {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>("");
+  const [showAlert, setShowAlert] = useState<boolean>(false);
 
   const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    
     if (!newTaskValue.trim()) {
-    return;
-  }
+      setShowAlert(true);
+      return;
+    }
+
     await addTodo({
       id: uuidv4(),
       text: newTaskValue,
@@ -24,6 +28,12 @@ const AddTask = () => {
     setNewTaskValue("");
     setModalOpen(false);
     router.refresh();
+  };
+
+ 
+  const handleInputChange = (e) => {
+    setNewTaskValue(e.target.value);
+    setShowAlert(false);
   };
 
   return (
@@ -41,7 +51,7 @@ const AddTask = () => {
           <div className='modal-action'>
             <input
               value={newTaskValue}
-              onChange={(e) => setNewTaskValue(e.target.value)}
+              onChange={handleInputChange} 
               type='text'
               placeholder='Type here'
               className='input input-bordered w-full'
@@ -50,6 +60,12 @@ const AddTask = () => {
               Submit
             </button>
           </div>
+          {showAlert && (
+            <div role="alert" className="alert alert-warning mt-4">
+              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+              <span>Warning: Task description cannot be empty!</span>
+            </div>
+          )}
         </form>
       </Modal>
     </div>
